0b62ae59ebb5937abb601554e7f7eef29fa89db7
Simon <shwaldemar@gmail.com>
2019-08-11 15:05:41 +0100

    classes created functions trimmed import and export working

--- client/src/data/instrument_families.js
+++ client/src/data/instrument_families.js
@@ -26,4 +26,4 @@ const instrumentFamilies = [
   }
 ];
 
-module.exports = instrumentFamilies;
+export default instrumentFamilies;
--- client/src/views/select_view.js
+++ client/src/views/select_view.js
@@ -1,28 +1,32 @@
-const PubSub = require('../helpers/pub_sub.js');
+import PubSub from '../helpers/pub_sub.js';
 
-const SelectView = function (element) {
+class SelectView {
+
+  constructor(element){
   this.element = element;
+  }
+
+  bindEvents() {
+    PubSub.subscribe('InstrumentFamilies:data-ready', (evt) => {
+      const allInstrumentFamilies = evt.detail;
+      this.populate(allInstrumentFamilies);
+    });
+
+
+  addEventListener('change', (evt) => {
+      const selectedIndex = evt.target.value;
+      PubSub.publish('SelectView:change', selectedIndex);
+    });
+  };
+
+  populate(instrumentFamilyData) {
+    instrumentFamilyData.forEach((familiy, index) => {
+      const option = document.createElement('option');
+      option.textContent = familiy.name;
+      option.value = index;
+      this.element.appendChild(option);
+    });
+  };
 };
 
-SelectView.prototype.bindEvents = function () {
-  PubSub.subscribe('InstrumentFamilies:data-ready', (evt) => {
-    const allInstrumentFamilies = evt.detail;
-    this.populate(allInstrumentFamilies);
-  });
-
-  this.element.addEventListener('change', (evt) => {
-    const selectedIndex = evt.target.value;
-    PubSub.publish('SelectView:change', selectedIndex);
-  });
-};
-
-SelectView.prototype.populate = function (instrumentFamilyData) {
-  instrumentFamilyData.forEach((familiy, index) => {
-    const option = document.createElement('option');
-    option.textContent = familiy.name;
-    option.value = index;
-    this.element.appendChild(option);
-  });
-};
-
-module.exports = SelectView;
+export default SelectView;
